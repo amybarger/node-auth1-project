4ac1da5c5b69048a587a14a6139a3b9e64509bd0
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-09-05 22:12:03 -0400

    Debugging model and router issues

---
+++ users/users-model.js
@@ -0,0 +1,26 @@
+const db = require("../database/config");
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return findById(id);
+}
+
+function findby(filter) {
+  return db("users")
+    .select("id", "username", "password")
+    .where(filter);
+}
+
+function findById(id) {
+  return db("users")
+    .select("id", "username")
+    .where({ id })
+    .first();
+}
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
